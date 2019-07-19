0bffb8c762b28c7d3c8aa971a562fd35cbe94390
Julius Alvarado <javascript.uiux@gmail.com>
2019-07-18 18:39:15 -0700

    added AppGlobals to try to set up debugging

--- src/classes/RsmEncodeRemove.php
+++ src/classes/RsmEncodeRemove.php
@@ -6,5 +6,28 @@ namespace Redstone\Tools;
 
 class RsmEncodeRemove
 {
+    /**
+     * @var string
+     */
+    private $path2file;
+    /**
+     * @var array
+     */
+    private $csvData;
     
+    public function __construct(string $directory, string $fileName) {
+        $this->path2file = $directory . DIRECTORY_SEPARATOR . $fileName;
+        $this->csvData = CsvParseModel::specificCsv2array($directory, $fileName);
+    }
+    
+    /**
+     * will parse CSV data, then export the clean data to a CSV file
+     */
+    public function removeEncodedChars(): void  {
+    
+    }
+    
+    public function getCleanFilePath(): string {
+    
+    }
 }
--- src/routes.php
+++ src/routes.php
@@ -6,11 +6,26 @@ use Slim\Http\Request;
 use Slim\Http\Response;
 use Redstone\Tools\RsmUploader;
 use Redstone\Tools\RsmEncodeRemove;
+use Redstone\Tools\AppGlobals;
+
+if(!empty(AppGlobals::$NINJA_AUTO_DEBUG) && AppGlobals::$NINJA_AUTO_DEBUG) {
+    /*
+        .17/.../street-view/user/mhetauser!@/lindsey@rsmail.com
+        .17/.../comauto/start/a/{action}
+    
+        //-- To do a "job board data mash" sql server insert:
+        .17/.../comauto/start/a/run?precision=exact&comauto-sql-insert=2
+    */
+    
+    $_SERVER['REQUEST_URI'] = '/';
+    $_SERVER['REQUEST_METHOD'] = 'POST';
+}
 
 return function(App $app) {
     
     $container = $app->getContainer();
     
+    // route root
     $app->post('/',
         function(Request $request, Response $response) use ($container, $app) {
             $directory = $container->get('upload_directory');
@@ -21,27 +36,32 @@ return function(App $app) {
             if($file->getError() === UPLOAD_ERR_OK) {
                 
                 $fileName = RsmUploader::moveUploadedFile($app, $directory, $file);
-                $filePath = $directory . DIRECTORY_SEPARATOR . $fileName;
+                $encodeRemove = new RsmEncodeRemove($directory, $fileName);
+                $encodeRemove->removeEncodedChars();
+                $cleanFile = $encodeRemove->getCleanFilePath();
                 
-                //-- headers to change:
+                //-- headers to change to download a file --\\
                 $cacheControl = 'Cache-Control';
                 $contentDescription = 'Content-Description';
                 $contentDisposition = 'Content-Disposition';
                 $contentType = 'Content-Type';
                 $contentTranserEncoding = 'Content-Transfer-Encoding';
+                
                 // test file path to see if this works
                 $testFile = 'C:\xampp\htdocs\@ Good Prac Data' . DIRECTORY_SEPARATOR . "test.csv";
                 
                 $response = $response->withHeader($cacheControl, 'public');
                 $response = $response->withHeader($contentDescription, 'File Transfer');
-                $response = $response->withHeader($contentDisposition, "attachment; filename=RedstoneFile.csv");
+                $response = $response->withHeader($contentDisposition, "attachment; filename=encodes_removed.csv");
                 $response = $response->withHeader($contentType, 'application/zip');
                 $response = $response->withHeader($contentTranserEncoding, 'binary');
                 
-                readfile($testFile); 
+                readfile($cleanFile);
                 
-                // send back info to user
                 return $response;
+            }
+            else {
+                exit("ERROR UPLOADING FILE - " . (string)$file->getError());
             }
         }
     );
