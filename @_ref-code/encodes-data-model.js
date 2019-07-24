018367def638ab65971478721b7b0ffa3f94ca3e
Julius Alvarado <javascript.uiux@gmail.com>
2019-07-23 20:04:31 -0700

    removed encodes rendered to view

---
+++ @_ref-code/encodes-data-model.js
@@ -0,0 +1,14 @@
+
+
+/*
+angularjs_id: "ng3321592l"
+created_at: "2019-07-23 19:43:48.630"
+encode2: "227"
+first_field: "11078"
+id: "4490"
+rsm_column: "1"
+rsm_file_name: "21127fd3076cfb87"
+rsm_row: "1"
+*/
+
+//
