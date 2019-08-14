c1bf09379662515e2d19b63c828d46d94f7ac07f
Julius Alvarado <javascript.uiux@gmail.com>
2019-08-13 20:23:53 -0700

    refactoring to OOP to ensure complete control of scope

--- _comingle/index.php
+++ _comingle/index.php
@@ -4,33 +4,54 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 use ParseCsv\Csv;
 
-$allCsvs = scandir('./csv');
 
-//TODO: refactor to a vector
-$csvMingle = [
-    // field title row
-    ['job_name', 'address', 'city', 'state', 'zip']
-];
-
-foreach($allCsvs as $c) {
-    // do not the relative symbols
-    if($c !== "." && $c !== "..") {
-        $csv = new Csv('./csv/'.$c);
-        $csvMingle [] = extractFields($csv['data'], $c);
-    }
-    // free memory
-    unset($csv);
-}
-
-function extractFields (array $data, string $fileName): array {
-    $extracted = [];
-    $fileName = str_replace('.csv', '', $fileName);
+class ComingleCombine
+{
+    private $extracted;
     
-    return $extracted;
-}
+    public function startExtract() {
+        $allCsv = scandir('./csv');
 
+        //TODO: refactor to a vector
+        $csvMingle = [
+            // field title row
+            ['job_name', 'address', 'city', 'state', 'zip'],
+        ];
+        foreach($allCsv as $c) {
+            // do not the relative symbols
+            if($c !== "." && $c !== "..") {
+                // use test dir to see what output csv looks like
+                $csv = new Csv('./test/' . $c);
+                
+                // will mutate $this->extracted
+                $this->extractFields($csv->data, $c);
+                
+                $break = 'point';
+            }
+            
+            // free memory
+            unset($csv);
+        }
+    }
+    
+    public function extractFields(array $data, string $fileName): void {
+        $extracted = [];
+        $filename = str_replace('.csv', '', $fileName);
+        
+        array_walk($data, function($item, $key, $filename) {
+            $this->extracted [] = $filename;
+            $this->extracted  [] = $item['address'];
+            $this->extracted  [] = $item['city'];
+            $this->extracted  [] = $item['state'];
+            $this->extracted  [] = $item['zip'];
+        }, $filename);
+    }
+    
+} // END OF: class ComingleCombine
 
+$comingle = new ComingleCombine();
 
+$comingle->startExtract();
 
 $break = 'point';
 
