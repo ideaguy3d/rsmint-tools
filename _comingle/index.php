f8524e4008606c754cbc84642a9472aad4b8a29c
Julius Alvarado <javascript.uiux@gmail.com>
2019-08-13 19:50:53 -0700

    loop to combine all the csv's set up

--- _comingle/index.php
+++ _comingle/index.php
@@ -4,8 +4,30 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 use ParseCsv\Csv;
 
-$csv = new Csv('./csv/test.csv');
+$allCsvs = scandir('./csv');
 
+//TODO: refactor to a vector
+$csvMingle = [
+    // field title row
+    ['job_name', 'address', 'city', 'state', 'zip']
+];
+
+foreach($allCsvs as $c) {
+    // do not the relative symbols
+    if($c !== "." && $c !== "..") {
+        $csv = new Csv('./csv/'.$c);
+        $csvMingle [] = extractFields($csv['data'], $c);
+    }
+    // free memory
+    unset($csv);
+}
+
+function extractFields (array $data, string $fileName): array {
+    $extracted = [];
+    $fileName = str_replace('.csv', '', $fileName);
+    
+    return $extracted;
+}
 
 
 
