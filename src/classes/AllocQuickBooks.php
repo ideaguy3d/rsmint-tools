2ae860a4477b1e3227315080635e3e26ab1f55c1
Julius Alvarado <javascript.uiux@gmail.com>
2019-12-09 18:16:10 -0800

    working on aggregating the Purchase Orders and Item Receipts

--- src/classes/AllocQuickBooks.php
+++ src/classes/AllocQuickBooks.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Redstone\Tools;
 
+use stdClass;
+
 /**
  * Class AllocQuickBooks as of 11-27-2019 this class is SUPER HARDCODED and not meant to be
  * used by anyone other than me.
@@ -13,11 +15,80 @@ namespace Redstone\Tools;
 class AllocQuickBooks
 {
     private $downloadsFolder;
+    private $receivedItems;
+    private $qbPurchaseOrderMap;
+    private $field;
+    private $rawHeaderRow;
+    /**
+     * This will be an enum for important fields from the raw file
+     *
+     * @var stdClass
+     */
+    private $fieldTitles;
     
     public function __construct() {
         $localDownloads = 'C:\Users\julius\Downloads';
         $proDownloads = 'C:\Users\RSMADMIN\Downloads';
         $isLocal = AppGlobals::isLocalHost();
+        $this->rawHeaderRow = [
+            0 => 'PO Number',
+            1 => 'Required By',
+            2 => 'Ship Method',
+            3 => 'Ordered By',
+            4 => 'Order Prepared',
+            5 => 'Value',
+            6 => 'Warehouse Name',
+            7 => 'Project Number',
+            8 => 'Purchase Order Terms',
+            9 => 'Purchase Order Notes',
+            10 => 'SKU',
+            11 => 'Ordered Qty',
+            12 => 'Received Qty',
+            13 => 'UOM',
+            14 => 'Unit Cost',
+            15 => 'Sales Price',
+            16 => 'Description',
+            17 => 'UPC / EAN',
+            18 => 'Category',
+            19 => 'Weight',
+            20 => 'Default Econ Order',
+            21 => 'Default Lead Time (Days)',
+            22 => 'Recommended Retail Price',
+            23 => 'Manufacturer Website',
+            24 => 'Serializable',
+            25 => 'Non-Inventory',
+            26 => 'Perishable',
+            27 => 'Track Lot',
+            28 => 'Size',
+            29 => 'Color',
+            30 => 'Window?',
+            31 => 'Special',
+            32 => 'Builds',
+            33 => 'Reserved',
+            34 => 'Supplier',
+            35 => 'Supplier Address 1',
+            36 => 'Supplier Address 1',
+            37 => 'Supplier City',
+            38 => 'Supplier State',
+            39 => 'Supplier Zip',
+            40 => 'Supplier Country',
+            41 => 'Supplier Main Contact',
+            42 => 'Supplier Phone',
+            43 => 'Supplier Email',
+            44 => 'Supplier Alternative Contact',
+            45 => 'Supplier Phone',
+            46 => 'Supplier Email',
+            47 => 'Supplier Office Phone',
+            48 => 'Supplier Office Fax',
+            49 => 'Supplier Website',
+            50 => 'Supplier Account Number',
+            51 => 'Supplier Terms',
+        ];
+        
+        $this->fieldTitles = new class() {
+            public $receivedQty = 'Received Qty';
+            public $poNum = 'PO Number';
+        };
         
         if($isLocal) {
             $this->downloadsFolder = $localDownloads;
@@ -47,9 +118,9 @@ class AllocQuickBooks
         // qb maps
         $qbHeaderRowStr = "Vendor,Transaction Date,PO Number,Item,Quantity,Description,Rate";
         $qbHeaderRow = explode(",", $qbHeaderRowStr);
-        $qbMap = [$qbHeaderRow];
+        $qbPurchaseOrderMap = [$qbHeaderRow];
         
-        // get each downloaded po file from Allocadence
+        // get each downloaded inboundexportbydate file from Allocadence
         foreach($downloadedFiles as $file) {
             $isPoFile = (strpos($file, $poFileName) !== false);
             if($isPoFile) {
@@ -66,6 +137,7 @@ class AllocQuickBooks
             // get header row real quick
             if($c === 0) {
                 $field = $this->poFindKeys($poArray[0]);
+                $this->field = $field;
                 $c++;
             }
             
@@ -85,28 +157,35 @@ class AllocQuickBooks
                 $_sku = trim($po[$field['SKU']]);
                 $_description = trim($po[$field['Description']]);
                 $_warehouse = trim($po[$field['Warehouse Name']]);
+                $_received = (int)trim($po[$field['Received Qty']]);
+                
+                if($_received > 0) {
+                    $this->receivedItems [] = $po;
+                }
                 
                 $value1 = $po[$field['Value']];
                 $value = str_replace(',', '', $value1);
                 $_value = (float)$value;
                 
                 $_qbDescription = "sku: $_sku, $_description, $_supplier, Category: $_category, Amount: $ {$value1}"
-                . " for $_warehouse";
+                    . " for $_warehouse";
                 
-                $qbMap[] = [
+                $qbPurchaseOrderMap [] = [
                     'Vendor' => $this->qbMapVendor($_supplier),
                     'Transaction Date' => $_requiredBy,
                     'PO Number' => $_poNumber,
                     'Item' => ($_category === 'E' ? 'Envelopes' : 'Paper'),
                     'Quantity' => number_format($_orderedQty),
                     'Description' => $_qbDescription,
-                    'Rate' => (round($_value /$_orderedQty, 7)),
+                    'Rate' => $_orderedQty !== 0 ? (round($_value / $_orderedQty, 7)) : 0,
                 ];
             }
             
         } // end of the main loop
         
-        CsvParseModel::export2csv($qbMap, './', 'qb_mapped_po');
+        $this->qbPurchaseOrderMap = $qbPurchaseOrderMap;
+        
+        CsvParseModel::export2csv($qbPurchaseOrderMap, './', 'qb_mapped_po');
         
     } // END OF: qbPurchaseOrderMap()
     
@@ -122,12 +201,23 @@ class AllocQuickBooks
      * @return array
      */
     private function poFindKeys(array $rawHeaderRow): array {
-        $headerRow = "PO Number,Required By,Value,Warehouse Name,SKU,Ordered Qty,Received Qty,Description,Category,Supplier";
+        $headerRow = "PO Number,Required By,Value,Warehouse Name,SKU,Ordered Qty,Received Qty,Description,Category,Supplier,";
         // the field indexes we want
         $poWantedFields = explode(',', $headerRow);
         $indexes = [];
-        foreach($poWantedFields as $f) {
-            $indexes[$f] = array_search($f, $rawHeaderRow);
+        $findAllFieldIndexes = true;
+        
+        if($findAllFieldIndexes) {
+            // get the field index for all the fields
+            foreach($rawHeaderRow as $field) {
+                $indexes[$field] = array_search($field, $rawHeaderRow);
+            }
+        }
+        else {
+            // just get indexes for the wanted fields
+            foreach($poWantedFields as $f) {
+                $indexes[$f] = array_search($f, $rawHeaderRow);
+            }
         }
         
         return $indexes;
@@ -151,7 +241,7 @@ class AllocQuickBooks
             'Volume Press',
             'Wilmer',
         ];
-    
+        
         $supplier = strtolower($allocSupplier);
         $supplier = substr($supplier, 0, strpos($supplier, ' '));
         
@@ -167,9 +257,31 @@ class AllocQuickBooks
     }
     
     /**
-     * Map Allocadence Receiving to QuickBooks
+     * Map Allocadence Receiving to QuickBooks, the function will mutate the
+     * class field $receivedItems
      */
-    public function qbReceivingMap() {
+    public function qbReceivingMap(): void {
         $break = 'point';
+        $items = [];
+        $qbItemReceiptStr = "Vendor,Transaction Date,RefNumber,Item,Description	Qty	Cost,Amount	PO No.";
+        $qbItemReceiptHeaderRow = explode(",", $qbItemReceiptStr);
+        $qbItemReceiptMap = ['header_row' => $qbItemReceiptHeaderRow];
+        
+        $f = $this->field;
+        $t = $this->fieldTitles;
+        
+        foreach($this->receivedItems as $receipt) {
+            $_receivedQty = $receipt[$f[$t->receivedQty]];
+            $_poNum = $receipt[$f[$t->poNum]];
+            
+            if(isset($items[$_poNum])) {
+                $items[$_poNum]['Amount'] += $_receivedQty; 
+            }
+            else {
+                $items[$_poNum] = [
+                    'Vendor'
+                ];
+            }
+        }
     }
 }
